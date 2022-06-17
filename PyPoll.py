47f67627baf6847f8d215b234bac673c074d3183
yingko123 <yingko123@yahoo.com>
2022-06-16 20:58:40 -0500

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -64,48 +64,67 @@ with open(file_to_load) as election_data:
         # Add a vote to that candidate's count
         candidate_votes[candidate_name] += 1
 
-
 print(candidate_options)
 print(candidate_votes)
-print(total_votes)      
+print(total_votes)  
 
 # Define variables and set to zero for winning count tracker
 winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
 
-# Determine the percentage of votes for each candidate by
-# looping through the {name:votes} candidate_votes dictionary
-for candidate_name in candidate_votes:
-    # Retrieve vote count of a candidate
-    votes = candidate_votes[candidate_name]
-    # Calculate the percentage of votes | float(convert integer to floating-point) | here gave same result
-    vote_percentage = float(votes) / float(total_votes) *100
-    # Print the candidate name and percentage of votes
-    print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
-
-    # Winning candidate and winning count tracker
-    if (votes>winning_count) and (vote_percentage>winning_percentage):
-        winning_count = votes
-        winning_percentage = vote_percentage
-        winning_candidate = candidate_name
-
-# Set up winning candidate summary
-winning_candidate_summary = (
-    f"--------------------------------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning vote count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"--------------------------------------------------\n")
-
-print(winning_candidate_summary)
-
-
 # Write[Output] analysis to text file
-##with open(file_to_save,"w") as outfile:
-    ##outfile.write("Counties in the Election\n")
-    ##outfile.write("--------------------------\n")
-    ##outfile.write("Arapahoe\nDenver\nJefferson")
+with open(file_to_save,"w") as txt_file:
+
+    # Print the final vote count to the terminal
+    election_results = (
+        f"\nElection Results\n"
+        f"----------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"----------------------------\n")
+    print(election_results,end="")
+
+    # Save the final vote count to the text file
+    txt_file.write(election_results)
+
+    # Determine the percentage of votes for each candidate by
+    # looping through the {name:votes} candidate_votes dictionary
+    for candidate_name in candidate_votes:
+        # Retrieve vote count of a candidate
+        votes = candidate_votes[candidate_name]
+        # Calculate the percentage of votes | float(convert integer to floating-point) | here gave same result
+        vote_percentage = float(votes) / float(total_votes) *100
+        # Print the candidate name and percentage of votes
+        print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+
+        # Print each candidate's election results
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        print (candidate_results)
+
+        # Save the candidate results to our text file
+        txt_file.write (candidate_results)
+
+        # Winning candidate and winning count tracker
+        if (votes>winning_count) and (vote_percentage>winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+
+    # Set up winning candidate summary
+    winning_candidate_summary = (
+        f"--------------------------------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning vote count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"--------------------------------------------------\n")
+
+    print(winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)
+
+
+    ##txt_file.write("Counties in the Election\n")
+    ##txt_file.write("--------------------------\n")
+    ##txt_file.write("Arapahoe\nDenver\nJefferson")
 
 
 
